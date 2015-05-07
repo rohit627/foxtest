ac3ced4b00d1bdbccf1d18537765dd720c0962df
Rohit Sharma <rohitsharma@Rohits-MacBook-Air.local>
2015-05-07 15:43:57 +0530

    centered banner using jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 (function() {
+    $('.header').css('padding-top', function() {
+        if($(window).width() <= 480) {
+            return Math.floor($(window).height()/3);
+        }
+        return Math.floor($(window).height()/3.3);
+    })
     // $('#content').height($(window).height());
     // $('#content').width($(window).width());
     // $( window ).resize(function() {
